d05298a5d9f6207a5ff32b4fb6bc0ab81e89f235
aldomq <58537416+aldomq@users.noreply.github.com>
2026-04-20 22:03:12 -0600

    TIenda-master file deleted
    Used tienda-master project to added code from there to the lavacar

--- _referencia_tienda/tienda-master/src/main/java/com/tienda/repository/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tienda.repository;
-
-import com.tienda.domain.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Integer>{
-    
-}
